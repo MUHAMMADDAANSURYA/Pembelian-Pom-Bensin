813ec75708f6948db8e58b0e1cbbfb65499e9892
MUHAMMADDAANSURYA <163943961+MUHAMMADDAANSURYA@users.noreply.github.com>
2024-07-04 11:50:38 +0700

    pweb

--- simpan_transaksi.php
+++ simpan_transaksi.php
@@ -2,12 +2,13 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
+    $jenis = htmlspecialchars($_POST['jenishbahanbakar']);
     $jumlah = htmlspecialchars($_POST['jumlah(liter)']);
     $harga = htmlspecialchars($_POST['harga(liter)']);
     $total = htmlspecialchars($_POST['totalharga']);
     $waktu = htmlspecialchars($_POST['waktu']);
 
-    $pesan = "Nama: $name\nEmail: $email\nJumlahperLiter: $jumlah\nHargaperLiter: $harga\nTotalharga: $total\nWaktupesan: $waktu\n\n";
+    $pesan = "Nama: $name\nEmail: $email\nJumlahperLiter: $jenis\nJenisBahanBakar: $jumlah\nHargaperLiter: $harga\nTotalharga: $total\nWaktupesan: $waktu\n\n";
     file_put_contents('transaksi.txt', $pesan, FILE_APPEND);
 
     echo "Pesanan Sukses Terimakasih";
